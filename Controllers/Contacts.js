ad064bc27e3a4731606d73b3468f1fb18867ca50
unknown <sashapivets@gmail.com>
2022-08-22 17:20:45 +0300

    new method

--- Controllers/Contacts.js
+++ Controllers/Contacts.js
@@ -1,30 +1,29 @@
-// const modelContacts = require("../models/contacts");
 const Contact = require("../models/contacts");
-// const createError = require("../helpers");
+const createError = require("../helpers");
 const constactsSchema = require("../Schemas/Schema");
 
 class Contacts {
-  // async listContacts(req, res, next) {
-  //   try {
-  //     const result = await Contact.find();
-  //     res.json(result);
-  //   } catch (error) {
-  //     next(error);
-  //   }
-  // }
+  async listContacts(req, res, next) {
+    try {
+      const result = await Contact.find();
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
 
-  // async getContactById(req, res, next) {
-  //   try {
-  //     const { contactId } = req.params;
-  //     const result = await modelContacts.getContactById(contactId);
-  //     if (!result) {
-  //       throw createError(404);
-  //     }
-  //     res.json(result);
-  //   } catch (error) {
-  //     next(error);
-  //   }
-  // }
+  async getContactById(req, res, next) {
+    try {
+      const { contactId } = req.params;
+      const result = await Contact.findById(contactId);
+      if (!result) {
+        throw createError(404);
+      }
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
 
   async addContact(req, res, next) {
     try {
@@ -40,37 +39,37 @@ class Contacts {
     }
   }
 
-  // async removeContact(req, res, next) {
-  //   try {
-  //     const { contactId } = req.params;
-  //     const result = await modelContacts.removeContact(contactId);
-  //     if (!result) {
-  //       throw createError(404);
-  //     }
+  async removeContact(req, res, next) {
+    try {
+      const { contactId } = req.params;
+      const result = await Contact.findByIdAndDelete(contactId);
+      if (!result) {
+        throw createError(404);
+      }
 
-  //     res.status(201).json({ message: "Contact deleted" });
-  //   } catch (error) {
-  //     next(error);
-  //   }
-  // }
+      res.status(201).json({ message: "Contact deleted" });
+    } catch (error) {
+      next(error);
+    }
+  }
 
-  // async updateContact(req, res, next) {
-  //   try {
-  //     const { error } = constactsSchema.validate(req.body);
-  //     if (error) {
-  //       res.status(400).json({ message: "missing fields" });
-  //       return;
-  //     }
-  //     const { contactId } = req.params;
-  //     const result = await modelContacts.updateContact(contactId, req.body);
-  //     if (!result) {
-  //       throw createError(404);
-  //     }
-  //     res.status(201).json(result);
-  //   } catch (error) {
-  //     next(error);
-  //   }
-  // }
+  async updateContact(req, res, next) {
+    try {
+      const { error } = constactsSchema.validate(req.body);
+      if (error) {
+        res.status(400).json({ message: "missing fields" });
+        return;
+      }
+      const { contactId } = req.params;
+      const result = await Contact.findByIdAndUpdate(contactId, req.body);
+      if (!result) {
+        throw createError(404);
+      }
+      res.status(201).json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = new Contacts();
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,10 +2,10 @@ const express = require("express");
 const Contacts = require("../../Controllers/Contacts");
 const router = express.Router();
 
-// router.get("/", Contacts.listContacts);
-// router.get("/:contactId", Contacts.getContactById);
+router.get("/", Contacts.listContacts);
+router.get("/:contactId", Contacts.getContactById);
 router.post("/", Contacts.addContact);
-// router.delete("/:contactId", Contacts.removeContact);
-// router.put("/:contactId", Contacts.updateContact);
+router.delete("/:contactId", Contacts.removeContact);
+router.put("/:contactId", Contacts.updateContact);
 
 module.exports = router;
